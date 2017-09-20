a6132cbec4c1423d71fd96c38d88ff594523b686
xc-racer99 <xc-racer2@live.ca>
2017-09-20 13:18:56 -0700

    Add in leptonica

--- leptonica-1.74.4.cmake
+++ leptonica-1.74.4.cmake
@@ -1,6 +1,5 @@
-# This file is part of OpenOrienteering.
-
 # Copyright 2016, 2017 Kai Pastor
+# Copyright 2017 Jonathan Bakker
 #
 # Redistribution and use is allowed according to the terms of the BSD license:
 #
@@ -27,43 +26,30 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-set(version        master)
-set(qt_version     5.6.2)
+set(version        1.74.4-cc)
+set(download_hash  SHA256=b05c7efd2ad05e2a77a052c70a6adcaa8b5c41df74d92d718fad7fd2c37074c6)
 
 superbuild_package(
-  NAME           android-camera-timing-pc
+  NAME           leptonica
   VERSION        ${version}
   DEPENDS
-    leptonica
-    opencv
-    qtbase-${qt_version}
-    qtimageformats-${qt_version}
-	qtserialport-${qt_version}
+    libpng
+    libjpeg-turbo
+    tiff
     zlib
-    host:qttools-${qt_version}
   
   SOURCE
-    DOWNLOAD_NAME  android-camera-timing-pc_${version}.tar.gz
-    URL            https://github.com/xc-racer99/android-camera-timing-pc/archive/${version}.tar.gz
-  
+    DOWNLOAD_NAME  leptonica-${version}.tar.gz
+	URL            https://github.com/xc-racer99/leptonica/archive/${version}.tar.gz
+    URL_HASH       ${download_hash}
+
+  PATCH_COMMAND
+    
+
   BUILD [[
     CMAKE_ARGS
       "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
-      "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-      "-DBUILD_SHARED_LIBS=0"
     INSTALL_COMMAND
-      "${CMAKE_COMMAND}" --build . --target install -- VERBOSE=1
-      $<$<BOOL:${WIN32}>:
-        # Windows installation layout is weird
-        "DESTDIR=${INSTALL_DIR}/CameraTimingAndroid-PC"
-      >$<$<NOT:$<BOOL:${WIN32}>>:
-        "DESTDIR=${INSTALL_DIR}"
-      >
-  ]]
-  
-  EXECUTABLES src/AndroidTimingPC
-  
-  PACKAGE [[
-    COMMAND "${CMAKE_COMMAND}" --build . --target package/fast
+      "${CMAKE_COMMAND}" --build . --target install/strip -- VERBOSE=1 "DESTDIR=${INSTALL_DIR}"
   ]]
 )
